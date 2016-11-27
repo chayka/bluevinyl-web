a3b7d61b51e2ba9659f9a4927744b00088345eff
Boris Mossounov <borix@tut.by>
2016-11-27 16:08:59 +0300

    Initial commit

--- server/index.js
+++ server/index.js
@@ -6,6 +6,6 @@ app.use(express.static('client/dist'));
 var admin = require('./routes/admin');
 app.use('/api/blue-vinyl', admin);
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
+app.listen(5000, function () {
+    console.log('Example app listening on port 5000!')
 });
